e88c0a29fb770e85a01498a0ecad51159ee7f7be
kubygithub <84660670+kubygithub@users.noreply.github.com>
2021-06-08 06:18:40 +0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-#author https://github.com/developeranaz (don't delete this)
+#author https://github.com/kubygithub (don't delete this)
 rclone version
 rclone config create 'CLOUDNAME' 'mega' 'user' $UserName 'pass' $PassWord
 rclone version
-rclone copy /donate-developeranaz.txt CLOUDNAME:
 rclone serve http CLOUDNAME: --addr :$PORT --buffer-size 256M --dir-cache-time 12h --vfs-read-chunk-size 256M --vfs-read-chunk-size-limit 2G --vfs-cache-mode writes
 
 
